9bac7ebb355cfe0da6166125fb7c3db30a662da8
GuzztMega <guzzt.mega@gmail.com>
2020-02-04 13:16:58 -0300

    Criado classe Produto e associação muitos para muitos

--- src/main/java/com/udemyspring/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/udemyspring/cursomc/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.udemyspring.cursomc.cursomc;
 
 import com.udemyspring.cursomc.cursomc.domain.Categoria;
+import com.udemyspring.cursomc.cursomc.domain.Produto;
 import com.udemyspring.cursomc.cursomc.repositories.CategoriaRepository;
+import com.udemyspring.cursomc.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
@@ -25,7 +30,20 @@ public class CursomcApplication implements CommandLineRunner {
         Categoria cat2 = new Categoria(null, "Escritório");
         Categoria cat3 = new Categoria(null, "Recursos Humanos");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Monitor", 500.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+        cat2.getProdutos().addAll(Arrays.asList(p1));
+        cat3.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
     }
 }
 
