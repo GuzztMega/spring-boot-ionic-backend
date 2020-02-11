c32dde188aee7cdd39bfcd052c633865182f0f2b
GuzztMega <guzzt.mega@gmail.com>
2020-02-11 09:43:04 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/udemyspring/cursomc/cursomc/domain/Produto.java
+++ src/main/java/com/udemyspring/cursomc/cursomc/domain/Produto.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Produto implements Serializable {
@@ -27,7 +25,20 @@ public class Produto implements Serializable {
             joinColumns = @JoinColumn(name = "produto_id"),
             inverseJoinColumns = @JoinColumn(name = "categoria_id")
     )
+
     private List<Categoria> categorias = new ArrayList<>();
+
+    @OneToMany(mappedBy = "id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
+
+    public List<Pedido> getPedidos(){
+        List<Pedido> lista = new ArrayList<>();
+        for (ItemPedido x : itens){
+            lista.add(x.getPedido());
+        }
+
+        return lista;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -80,4 +91,8 @@ public class Produto implements Serializable {
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
     }
+
+    public Set<ItemPedido> getItens() { return itens; }
+
+    public void setItens(Set<ItemPedido> itens) { this.itens = itens; }
 }
---
+++ src/main/java/com/udemyspring/cursomc/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.udemyspring.cursomc.cursomc.repositories;
+
+import com.udemyspring.cursomc.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+
+
+}
