6b3c391b82923991ec869a4f6f98506911b0be16
GuzzMega <guzzt.mega@gmail.com>
2020-03-21 14:28:32 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/udemyspring/cursomc/cursomc/config/DevConfig.java
+++ src/main/java/com/udemyspring/cursomc/cursomc/config/DevConfig.java
@@ -22,7 +22,7 @@ public class DevConfig {
     @Bean
     public boolean instantiateDatabase() throws ParseException {
 
-        if(!"create".equals(strategy)){
+        if(!"update".equals(strategy)){
             return false;
         }
         dbService.instantiateTestDataBase();
