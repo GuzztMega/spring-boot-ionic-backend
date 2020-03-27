2247174351fb83ef33663fe38aa5b619204f920a
Augusto Longo <guzzt.mega@gmail.com>
2020-03-27 14:41:31 -0300

    Email HTML com ThymeLeaf

--- src/main/java/com/udemyspring/cursomc/cursomc/services/EmailService.java
+++ src/main/java/com/udemyspring/cursomc/cursomc/services/EmailService.java
@@ -3,8 +3,13 @@ package com.udemyspring.cursomc.cursomc.services;
 import com.udemyspring.cursomc.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
