8b571a9b7bfc85badf62c4e5a8f160238180baa9
Augusto Longo <guzzt.mega@gmail.com>
2020-04-09 17:42:50 -0300

    Enviando imagem via endpoint

--- src/main/java/com/udemyspring/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/udemyspring/cursomc/cursomc/CursomcApplication.java
@@ -1,23 +1,12 @@
 package com.udemyspring.cursomc.cursomc;
 
-import com.udemyspring.cursomc.cursomc.domain.*;
-import com.udemyspring.cursomc.cursomc.domain.enums.EstadoPagamento;
-import com.udemyspring.cursomc.cursomc.domain.enums.TipoCliente;
-import com.udemyspring.cursomc.cursomc.repositories.*;
-import com.udemyspring.cursomc.cursomc.services.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
-    @Autowired
-    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +14,6 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        s3Service.uploadFile("C:\\Users\\AU40044421\\Pictures\\carro.jpg");
     }
 }
 
